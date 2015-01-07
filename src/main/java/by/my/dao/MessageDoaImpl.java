5f7eebbc09278f4cf575ca7e19e868e44e47b54f
njjnex <_nox@tut.by>
2015-01-07 13:21:20 -0300

    07.01 Message service

---
+++ src/main/java/by/my/dao/MessageDoaImpl.java
@@ -0,0 +1,33 @@
+package by.my.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import by.my.entity.Event;
+import by.my.entity.Message;
+
+@Repository
+public class MessageDoaImpl implements MessageDao {
+	@Autowired
+	SessionFactory sessionFactory;
+
+	@Override
+	public void save(Message message) {
+		sessionFactory.getCurrentSession().save(message);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Message> getEventMessages(Event event) {
+		return sessionFactory
+				.getCurrentSession()
+				.createQuery(
+						"FROM Message message WHERE message.event =:event")
+				.setParameter("event", event).list();
+
+	}
+
+}
